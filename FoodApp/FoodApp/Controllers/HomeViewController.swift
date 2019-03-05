dc93ed7c3ad1190d63bf0e6aead97083f525d0f2
gionoa <gnoa001@fiu.edu>
2019-03-05 14:09:46 -0500

    stuff

--- FoodApp/FoodApp/Controllers/HomeViewController.swift
+++ FoodApp/FoodApp/Controllers/HomeViewController.swift
@@ -28,7 +28,6 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         view.backgroundColor = UIColor.blue
         
         
